e9372241a7f61a07cfc5541d909ae41e0e78f296
Thibault Suzanne <thi.suzanne@gmail.com>
2020-09-07 21:11:25 +0200

    Update app with remaining gas information

--- app/src/app.ml
+++ app/src/app.ml
@@ -43,6 +43,12 @@ let apply_action model action _ ~schedule_action:_ = match (action : Action.t) w
 let on_startup ~schedule_action:_ _ =
   Async_kernel.return ()
 
+let bottom_tank = Tank.double_al80 Gas.air ()
+let deco_tanks = [Tank.al_7l (Gas.nx 50) (); Tank.al_7l Gas.oxy ()]
+let tanks = bottom_tank :: deco_tanks
+let gas_supply_full = Gas_supply.full_tanks tanks
+
+
 let one_segment_node segment =
   let open Vdom in
   Node.tr [Attr.class_ "mdc-data-table__row"] @@
@@ -53,16 +59,49 @@ let one_segment_node segment =
 let runtime_node profile =
   let open Vdom in
   let strings = Profile.to_strings profile in
-  Node.div [Attr.class_ "mdc-data-table"] [
-    Node.div [Attr.class_ "mdc-data-table__table-container"] [
-      Node.table [Attr.class_ "mdc-data-table__table"] [
-        Node.tbody [Attr.class_ "mdc-data-table__content"] @@
-        List.map ~f:one_segment_node strings
+  Node.div
+    [Attr.create "style" "display: flex; justify-content: center"]
+    [
+      Node.div [Attr.class_ "mdc-data-table"] [
+        Node.div [Attr.class_ "mdc-data-table__table-container"] [
+          Node.table [Attr.class_ "mdc-data-table__table"] [
+            Node.tbody [Attr.class_ "mdc-data-table__content"] @@
+            List.map ~f:one_segment_node strings
+          ]
+        ]
       ]
     ]
+
+let one_tank_supply_node gas_supply tank =
+  let open Vdom in
+  let remaining_pressure = Gas_supply.remaining_pressure tank gas_supply in
+  let remaining_normal_volume = Gas_supply.remaining_normal_volume tank gas_supply in
+  Node.li [Attr.class_"mdc-list-item"] [
+    Node.span [Attr.class_ "mdc-list-item__ripple"] [];
+    Node.span [Attr.class_ "mdc-list-item__text"] [
+      Node.span [Attr.class_ "mdc-list-item__primary-text"] [
+        Node.text @@
+        Fmt.str "%.0f bar of %a"
+          remaining_pressure
+          Gas.pp (Tank.gas tank)
+      ];
+      Node.span [Attr.class_ "mdc-list-item__secondary-text"] [
+        Node.text @@
+        Fmt.str "%.0f L in %a L tank"
+          (Physics.to_litre remaining_normal_volume)
+          (Fmt.float_dfrac 1) (Physics.to_litre @@ Tank.volume tank)
+      ];
+    ];
   ]
 
-let tank = Tank.double_al80 Gas.air ()
+let gas_supply_node gas_supply =
+  let open Vdom in
+  Node.section [] [
+    Node.h2 [Attr.class_ "mdc-typography--headline5"] [Node.text "Remaining gas"];
+    Node.ul [Attr.classes ["mdc-list"; "mdc-list--two-line"]] (
+      List.map ~f:(one_tank_supply_node gas_supply) tanks
+    )
+  ]
 
 let view m ~inject =
   let open Incr.Let_syntax in
@@ -102,20 +141,24 @@ let view m ~inject =
   let mdc_cell elt =
     Node.div [Attr.class_ "mdc-layout-grid__cell--span-2"] [elt]
   in
-  let%map runtime_node =
+  let%map full_profile =
     m >>| fun { Model.depth; time } ->
     let profile =
       Profile.square
         Param.default
-        ~tank
+        ~tank:bottom_tank
         ~depth ~time in
-    let tanks = [tank] in
     let gf = (0.8, 0.8) in
     let deco = Buhlmann.deco_procedure Param.default gf tanks profile in
-    let full_profile = Profile.append profile deco in
-    runtime_node full_profile
-    in
-    Node.div []
+    Profile.append profile deco
+  in
+  let runtime_node =
+    runtime_node full_profile in
+  let gas_supply =
+    Gas_supply.breathe_on_profile Param.default full_profile gas_supply_full in
+  let gas_supply_node =
+    gas_supply_node gas_supply in
+  Node.div []
     [
       Node.div
         [Attr.class_ "mdc-layout-grid"]
@@ -124,9 +167,8 @@ let view m ~inject =
             [Attr.class_ "mdc-layout-grid__inner"]
             [mdc_cell depth_input; mdc_cell time_input];
         ];
-      Node.div
-        [Attr.create "style" "display: flex; justify-content: center"]
-        [runtime_node]
+      runtime_node;
+      gas_supply_node;
     ]
 
 let on_display _state ~schedule_action:_ =
